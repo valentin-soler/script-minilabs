9eb5a7fc2b984e4304350f22460fb973e8e48b9a
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2025-06-30 10:25:16 +0200

    add -y missing

--- install_webmin.sh
+++ install_webmin.sh
@@ -4,6 +4,6 @@
 apt install curl
 curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
 sh webmin-setup-repo.sh
-apt install webmin --install-recommends
+apt install webmin --install-recommends -y
 
 echo "Webmin : ip:10000"
