9c717750059045ed6fc6dea613a45a1f7de05a36
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2025-06-30 10:19:26 +0200

    Install CURL Missing

--- install_webmin.sh
+++ install_webmin.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Repo install
+apt install curl
 curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
 sh webmin-setup-repo.sh
 apt install webmin --install-recommends
